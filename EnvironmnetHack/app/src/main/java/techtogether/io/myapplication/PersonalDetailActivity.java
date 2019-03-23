a580580e993352716f6d13eba046c0d1f26f4634
suryasruthi <m.suryasruthi@gmail.com>
2019-03-23 19:11:33 -0400

    firebase edits

--- EnvironmnetHack/app/src/main/java/techtogether/io/myapplication/PersonalDetailActivity.java
+++ EnvironmnetHack/app/src/main/java/techtogether/io/myapplication/PersonalDetailActivity.java
@@ -1,5 +1,6 @@
 package techtogether.io.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -8,7 +9,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -38,21 +41,31 @@ public class PersonalDetailActivity extends AppCompatActivity {
         final EditText birthday = findViewById(R.id.birthdayValTextView);
        // String email = getIntent().getExtras().getString(MainActivity.EMAIL);
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        
+
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //get values from textboxes + email
                 //do some inserts to firebase
 
-                mRootRef = FirebaseDatabase.getInstance().getReference().child("ecotrackr");
+                mRootRef = FirebaseDatabase.getInstance().getReference();
 
-                String id = mRootRef.child("ecotrackr").push().getKey();
+                String id = mRootRef.push().getKey();
                 User user = new User(name.getText().toString(), location.getText().toString(), gender.getText().toString(), birthday.getText().toString());
                 user.setName(name.getText().toString());
                 user.setLocation(location.getText().toString());
                 user.setGender(gender.getText().toString());
                 user.setBirthday(birthday.getText().toString());
-                mRootRef.child("ecotrackr").child("users").child(id).setValue(user);
+                mRootRef.child("users").child(id).setValue(user);
+                Toast.makeText(getApplicationContext(), "Added to the DB!", Toast.LENGTH_SHORT).show();
             }
         });
         signoutButton.setOnClickListener(new View.OnClickListener() {
